dafb021010a36ebed8533835df920dc4eb07268f
Jinglin82@gmail.com <Jinglin82@gmail.com>
2014-05-03 03:41:58 -0400

    Tank graphics

--- js/canvas.js
+++ js/canvas.js
@@ -1,3 +1,4 @@
+
 function randomInteger(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -44,22 +45,51 @@ function Tank(x,y,width,height,colorCode,colorStyle,context) {
 };
 
 Tank.prototype.draw = function() {
-	context.rect(this.x,this.y,this.width,this.height);
-	console.log(this.color);
+	context.fillStyle = this.color;
+	context.fillRect(this.x-5,this.y-7,10,44);
+	context.fillRect(this.x+25,this.y-7,10,44);
+	context.strokeRect(this.x-5,this.y-7,10,44);
+	context.strokeRect(this.x+25,this.y-7,10,44);
+	context.fillStyle = randomColor();
+	context.fillRect(this.x,this.y,this.width,this.height);
+	context.strokeRect(this.x,this.y,this.width,this.height);
+	var terrainTurret = new Turret(this.x+15,this.y+15,randomColor(),this.context);
+	terrainTurret.draw();
+
+};
+
+function Turret(x,y,colorCode,context) {
+	this.x = x; 
+	this.y = y;
+	this.color = colorCode;
+	this.context = context;
+};
+
+Turret.prototype.draw = function() {
+	var r = 12;
+	var coverPoints = [[r,0],
+				  [r*Math.cos(Math.PI/3),r*Math.sin(Math.PI/3)],
+				  [-r*Math.cos(Math.PI/3),r*Math.sin(Math.PI/3)],
+				  [-r,0],
+				  [-r*Math.cos(Math.PI/3),-r*Math.sin(Math.PI/3)],
+				  [r*Math.cos(Math.PI/3),-r*Math.sin(Math.PI/3)],
+				  [r,0]];
+	context.moveTo(this.x+coverPoints[0][0],this.y+coverPoints[0][1]);
+	for(var count = 1; count < 6; count++) {
+		context.lineTo(this.x+coverPoints[count][0],this.y+coverPoints[count][1]);
+	}
+	context.closePath();
 	context.fillStyle = this.color;
 	context.fill();
-	context.lineWidth = 2;
-}
+	context.strokeStyle = randomColor();
+	context.stroke();
+	context.fillStyle = randomColor();
+	context.fillRect(this.x-5,this.y-27,10,24);
+	context.strokeRect(this.x-5,this.y-27,10,24);
+};
 
-var terrainBoxes = [];
-for(var i = 0; i < 2; i++) {
-	terrainBoxes[i] = new Rectangle(randomInteger(0,40) + randomInteger(20,750), randomInteger(20,500), randomInteger(20,100), 
-									randomInteger(20,200), randomColor(), randomColor(), context);
-	terrainBoxes[i].draw();
-}
 
-var terrainTank = new Tank(randomInteger(0,40) + randomInteger(20,750), randomInteger(20,500), randomInteger(20,60),
- 					   	   randomInteger(20,60), randomColor(), randomColor(), context);
+var terrainTank = new Tank(randomInteger(20,700), randomInteger(20,460), 30, 30, randomColor(), randomColor(), context);
 terrainTank.draw();
 
 
