d8b2163efc8934d28f466f6cd87cf73bf693194a
Jinglin82@gmail.com <Jinglin82@gmail.com>
2014-06-05 21:03:13 -0400

    wallSpots

--- flow/js/world.js
+++ flow/js/world.js
@@ -17,20 +17,21 @@ var World = function() {
 		this.muds = [];
 		this.b = [];
 		for(var i in map.walls) {
+			var upperBound = (0.3+0.2*Math.random()) * (map.walls[i][2] * map.walls[i][3] / 400);
+			var ws = [];
+			for(var iter = 0; iter < upperBound; iter++) {
+				ws.push({
+					x : map.walls[i][0] + Math.random() * (map.walls[i][2] - 10),
+					y : map.walls[i][1] + Math.random() * (map.walls[i][3] - 10)
+				})
+			}
 			this.walls.push({
 				x : map.walls[i][0],
 				y : map.walls[i][1],
 				w : map.walls[i][2],
 				h : map.walls[i][3],
-				wallSpots : 0
+				wallSpots : ws			
 			})
-			var upperBound = map.walls[i][2] * map.walls[i][3] / 400 
-			for(var iter = 0, stop = Math.floor(Math.random() * upperBound); iter < stop; iter++) {
-				this.wallSpots.push({
-					x : map.walls[i][0] + Math.floor(Math.random() * (map.walls[i][2] - 10)),
-					y : map.walls[i][1] + Math.floor(Math.random() * (map.walls[i][3] - 10))
-				})
-			}
 		}
 		for(var i in map.lasers) {
 			this.lasers.push({
@@ -91,7 +92,6 @@ var World = function() {
 		for(var i in this.tanks) {
 			this.tanks[i].step();
 			this.tanks[i].draw(g);
-			this.tanks[i].fire();
 		}
 		for(var count = 0; count < this.walls.length; count++) {
 			g.fillStyle = "rgb(0,210,255)";
@@ -102,10 +102,13 @@ var World = function() {
 			// 			   this.walls[count].y + Math.floor(Math.random() * (this.walls[count].h - 10)),
 			// 			   4,4);
 			// }
+			for(var iter = 0; iter < this.walls[count].wallSpots.length; iter++) {
+				g.fillRect(this.walls[count].wallSpots[iter].x,this.walls[count].wallSpots[iter].y,4,4);
+			}
 		}
-		for(var count = 0; count < this.wallSpots.length; count++) {
-			g.fillRect(this.wallSpots[count].x,this.wallSpots[count].y,4,4);
-		}
+		// for(var count = 0; count < this.wallSpots.length; count++) {
+		// 	g.fillRect(this.wallSpots[count].x,this.wallSpots[count].y,4,4);
+		// }
 		if (!arrBullets.length) return;
 		for(var count = 0; count < arrBullets.length; count++)
 			if(arrBullets[count].p.x > 0 && arrBullets[count].p.x < 720 && 
