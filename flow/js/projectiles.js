fe5254494aadd0ed566a49ca845cb495a3a7b900
Jinglin82@gmail.com <Jinglin82@gmail.com>
2014-06-13 19:12:52 -0400

    Weapons

--- flow/js/projectiles.js
+++ flow/js/projectiles.js
@@ -48,6 +48,36 @@ var Bullet = Projectile.extend(function() {
 	});
 });
 
+var sniperBullet = Projectile.extend(function() {
+	this.s = new Dimension(WEAPONS.TURRET.SIZE[0],WEAPONS.TURRET.SIZE[1]);
+	this.init = function(position, angle, velocity) {
+		this.p = position.clone();
+		this.v = new Vector().addA(angle, velocity);
+		this.angle = angle;
+		return this;
+	}
+	this.steps.push(function() {
+		var mult = 1000;
+		if (this.W.FF.getColor(Math.floor(this.p.x/6),Math.floor(this.p.y/6)) > 1) {
+			this.v.addC(this.W.FF.getXVelocity(Math.floor(this.p.x/6),Math.floor(this.p.y/6))*mult,
+						this.W.FF.getYVelocity(Math.floor(this.p.x/6),Math.floor(this.p.y/6))*mult);
+		}
+		this.p.add(this.v);
+		if (this.W.col(0, this.p, this.s))
+			this.destroy = true;
+	});
+	this.draws.push(function(g) {
+		g.translate(this.p.x,this.p.y);
+			g.rotate(this.angle);
+				g.fillStyle = g.strokeStyle = "rgb(255,255,255)";
+				g.fillRect(0,0,this.s.x,this.s.y);
+				g.strokeRect(0,0,this.s.x,this.s.y);
+			g.rotate(-this.angle);
+		g.translate(-this.p.x,-this.p.y);
+	});
+});
+
+
 
 
 
--- flow/js/weapons.js
+++ flow/js/weapons.js
@@ -1,7 +1,32 @@
 var WEAPONS = {
 	TURRET : {
 		SPEED : 10,
-		SIZE : [6,3]
+		SIZE : [6,3],
+		DELAY : 3,
+		COLOR : {
+			'turret' : 'rgb(65,62,67)', 
+			'cover' : 'rgb(255,149,0)'
+		}
+	},
+	POWERTURRET : {
+		SPEED : 7,
+		SIZE : [7,3],
+		SPREAD : Math.PI / 8,
+		DELAY : 7,
+		COLOR : {
+			'turret' : 'rgb(72,253,229)',
+			'cover' : 'rgb(72,153,253)'
+		}
+	},
+	SNIPERTURRET : {
+		SPEED : 8,
+		SIZE : [6,3],
+		DELAY : 20,
+		ACCURACY : 30,
+		COLOR : {
+			'turret' : 'rgb(252,65,109)',
+			'cover' : 'rgb(251,155,177)'
+		}
 	}
 };
 
@@ -17,13 +42,13 @@ var Weapon = function() {
 var Turret = Weapon.extend(function() {
 	this.timer = 0;
 	this.fire = function() {
-		if (this.timer-- <= 0 && (this.timer = 3)) {
+		if (this.timer-- <= 0 && (this.timer = WEAPONS.TURRET.DELAY)) {
 			this.W.b.push((new Bullet()).init(this.T.cp.clone().addA(this.T.angle,15), this.T.angle, WEAPONS.TURRET.SPEED).setVars(this.W,this.T,this));
 		}
 	}
 	this.draw = function(g) {
-		g.fillStyle = TANKS.NORMAL.color['cover'];
-		g.strokeStyle = TANKS.NORMAL.color['turret'];
+		g.fillStyle = WEAPONS.TURRET.COLOR['cover'];
+		g.strokeStyle = WEAPONS.TURRET.COLOR['turret'];
 		g.fillRect(2,-3,18,6);
 		g.strokeRect(2,-3,18,6);
 		g.beginPath();
@@ -31,3 +56,43 @@ var Turret = Weapon.extend(function() {
 		g.fill();
 	}
 });
+
+var powerTurret = Weapon.extend(function() {
+	this.timer = 0;
+	this.fire = function() {
+		if(this.timer-- <= 0 && (this.timer = WEAPONS.POWERTURRET.DELAY)) {
+			this.W.b.push((new Bullet()).init(this.T.cp.clone().addA(this.T.angle,15), this.T.angle, WEAPONS.POWERTURRET.SPEED).setVars(this.W,this.T,this));
+			this.W.b.push((new Bullet()).init(this.T.cp.clone().addA(this.T.angle,15), this.T.angle + WEAPONS.POWERTURRET.SPREAD, WEAPONS.POWERTURRET.SPEED).setVars(this.W,this.T,this));
+			this.W.b.push((new Bullet()).init(this.T.cp.clone().addA(this.T.angle,15), this.T.angle - WEAPONS.POWERTURRET.SPREAD, WEAPONS.POWERTURRET.SPEED).setVars(this.W,this.T,this));
+		}
+	}
+	this.draw = function(g) {
+		g.fillStyle = WEAPONS.POWERTURRET.COLOR['cover'];
+		g.strokeStyle = WEAPONS.POWERTURRET.COLOR['turret'];
+		g.fillRect(2,-3,18,6);
+		g.strokeRect(2,-3,18,6);
+		g.beginPath();
+		g.arc(0,0,10,0,Math.PI*2,true);
+		g.fill();
+	}
+});
+
+var sniperTurret = Weapon.extend(function() {
+	this.timer = 0;
+	this.fire = function() {
+		if(this.timer-- <= 0 && (this.timer = WEAPONS.SNIPERTURRET.DELAY)) {
+			this.W.b.push((new Bullet()).init(this.T.cp.clone().addA(this.T.angle,15), this.T.angle, WEAPONS.POWERTURRET.SPEED).setVars(this.W,this.T,this));
+		}
+	}
+	this.draw = function(g) {
+		g.fillStyle = WEAPONS.SNIPERTURRET.COLOR['cover'];
+		g.strokeStyle = WEAPONS.SNIPERTURRET.COLOR['turret'];
+		g.fillRect(2,-3,18,6);
+		g.strokeRect(2,-3,18,6);
+		g.beginPath();
+		g.arc(0,0,10,0,Math.PI*2,true);
+		g.fill();
+	}
+});
+
+
