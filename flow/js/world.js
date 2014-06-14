0d3645209bb4bf3e1ad2f20fd458a08ab37df36b
rogerli <rog3rli@gmail.com>
2014-06-14 16:53:10 -0400

    JING STOP PUSHING SO LATE

--- flow/js/world.js
+++ flow/js/world.js
@@ -52,13 +52,8 @@ var World = function() {
 		for(var i in map.tanks) {
 			this.tanks.push( ((i==1)?(new Scout()):(new NormalTank()))
 				.setup(this, new Point(map.tanks[i][0],map.tanks[i][1]), map.tanks[i][2],
-<<<<<<< HEAD
-					(i==0)?([new Minigun(), new SpreadTurret()]):
-						([new Minigun(), new MineDropper()])
-=======
 					(i==1)?([new Minigun(), new Turret()]):
 						([new Laser(), new Minigun()])
->>>>>>> fc8c3eb9bcb0946239a2a6aa001e726a26e40508
 				));
 		}
 
