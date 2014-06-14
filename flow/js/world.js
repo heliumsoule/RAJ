9391c4fcd683c18d641e9f64ecb113c6ca758bb1
rogerli <rog3rli@gmail.com>
2014-06-14 11:56:02 -0400

    blah

--- flow/js/world.js
+++ flow/js/world.js
@@ -49,10 +49,10 @@ var World = function() {
 			})
 		}
 		for(var i in map.tanks) {
-			this.tanks.push( ((i==0)?(new SpikedTank()):(new NormalTank()))
+			this.tanks.push( ((i==1)?(new Scout()):(new NormalTank()))
 				.setup(this, new Point(map.tanks[i][0],map.tanks[i][1]), map.tanks[i][2],
-					(i==0)?([new MineDropper(), new TripleTurret()]):
-						([new Minigun(), new TripleTurret()])
+					(i==1)?([new Minigun(), new Turret()]):
+						([new Sniper(), new Minigun()])
 				));
 		}
 
